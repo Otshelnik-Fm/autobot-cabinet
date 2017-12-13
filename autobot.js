e8d90b94fc832e8481a9418670d18dbc51faa138
Otshelnik-Fm <Otshelnik-Fm@yandex.ru>
2017-12-13 15:45:44 +0400

    init

---
+++ autobot.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function() {
+    jQuery('#rcl-office .user-status,.chat-users-box,.chat-form').remove(); // удалим лишние блоки
+    
+    jQuery('#rcl-office[data-account="5"] .author-name').text('AutoBot');
+    
+    var count = jQuery('#ac_autobot .rcb_nmbr');
+    
+    var num = count.html(); // подсветим кол-во блоков. значение возьмем с иконки
+    if(num > 0){
+        jQuery('.chat-message:nth-last-child(-n+'+num+')').addClass('aub_active');
+    }
+    
+    jQuery(count).removeClass('counter_not_null').text('0'); // очистим счетчик
+});
